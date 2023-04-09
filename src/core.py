97cf6e43715c73505ec542984fab43b0b3cde77e
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-04-09 15:03:26 -0300

    testing align to equator and to site

--- src/core.py
+++ src/core.py
@@ -3,16 +3,29 @@ import datetime as dt
 import time
 import numpy as np
 import pandas as pd
+from scipy.signal import argrelmin
 
-coords = {
-    "car": (-7.38, -36.528),  # Cariri
-    "for": (-3.73, -38.522),  # Fortaleza
-    "caj": (-6.89, -38.56),   # Cajazeiras
-    "saa": (-2.53, -44.296),  # Sao Luis
-    "boa": ( 2.8,  -60.7),    # Boa Vista
-    "ccb": (-9.5,  -54.8),    # Cachimbo
-    "cgg": (-20.5, -54.7)     # Campo Grande
+sites = {
+    "car": {"coords": (-7.38, -36.528), 
+            "name": "Cariri"},  
+    "for": {"coords":  (-3.73, -38.522), 
+            "name": "Fortaleza"},
+    "caj": {"coords": (-6.89, -38.56), 
+            "name": "Cajazeiras"},
+    "saa": {"coords":  (-2.53, -44.296), 
+            "name": "Sao Luis"},
+    "boa": {"coords": ( 2.8,  -60.7), 
+            "name": "Boa Vista"},
+    "ccb": {"coords": (-9.5,  -54.8), 
+            "name": "Cachimbo"},
+    "cgg": {"coords":(-20.5, -54.7), 
+            "name": "Campo Grande"},
+    "jic": {"coords":(-11.95, -76.87), 
+            "name": "Jicamarca"} 
     }     
+
+
+
 
 def year_fraction(date):
     
@@ -50,10 +63,9 @@ def run_igrf(
         alt = 250, 
         ):
          
-    lat, lon = coords[site]
-    
-    #lon += 360
-    
+    lat, lon = sites[site]["coords"]
+    name = sites[site]["name"]
+        
     d, i, h, x, y, z, f = pyIGRF.igrf_value(
         lat, lon, 
         alt = alt, 
@@ -66,29 +78,87 @@ def run_igrf(
 def middle(array, m = 0):
     return array[int((len(array) - 1) /2) - m]
 
-def intersection_in_geo_equator(eq, xx, yy):
+
+
+
+def compute_distance(x, y, x0, y0):
+    
+    def distance(x, y, x0, y0):
+        d_x = x - x0
+        d_y = y - y0
+        dis = np.sqrt( d_x**2 + d_y**2 )
+        return dis
+    
+    # compute distance
+    dis = distance(x, y, x0, y0)
+    
+    # find the minima
+    min_idxs = argrelmin(dis)[0]
+    # take the minimum
+    glob_min_idx = min_idxs[np.argmin(dis[min_idxs])]
+    
+    # coordinates and distance
+    min_x = x[glob_min_idx]
+    min_y = y[glob_min_idx]
+    min_d = dis[glob_min_idx]
+    
+    return min_x, min_y, min_d
+
+def find_closest(arr, val):
+   idx = np.abs(arr - val).argmin()
+   return idx
+
+def intersection_in_equator(eq, xx, yy):
     
     lon_cond = (eq[:, 0] > xx[0]) & (eq[:, 0] < xx[-1]) 
     lat_cond = (eq[:, 1] < yy[0]) & (eq[:, 1] > yy[-1])
     
     ds = eq[np.where(lat_cond & lon_cond), :][0]
 
-    return tuple(middle(ds, m = 1))
+    return tuple(middle(ds, m = 0))
+
+
+def align_to_site(x, y, glon):
+    mx, my = middle(x), middle(y)
+    eq = load_equator()
+    
+    idx = find_closest(eq[:, 0], glon)
+    nx, ny = tuple(eq[idx, :])
+    
+    x -= abs(nx - mx)
+    y -= abs(ny - my)
+    
+    return x, y
+
+def align_to_equator(x, y, glon, glat):
+    
+    eq = load_equator()
+    
+    min_x, min_y, min_d = compute_distance(
+        eq[:, 0], eq[:, 1], glon, glat)
+    
+    delta_lon = abs(min_x - glon)
+    delta_lat = abs(min_y - glat)
+
+    x -= delta_lon
+    y += delta_lat
+    return x, y
 
 def compute_meridian(
         lon = -60, 
         max_lat = 20,
         alt = 0, 
-        year = 2013,
-        align_to_equator = True
+        year = 2013
         ):
     
     xx = []
     yy = []
     
-    delta = 1.0
+    delta = 1
+
+    range_lats = np.arange(-max_lat, max_lat, delta)[::-1]
     
-    for lat in np.arange(-max_lat, max_lat)[::-1]:
+    for lat in range_lats:
         d, i, h, x, y, z, f = pyIGRF.igrf_value(
             lat, 
             lon, 
@@ -104,19 +174,8 @@ def compute_meridian(
         
         xx.append(lon)
         yy.append(lat)
-        
-    if align_to_equator:
-    
-        eq = load_equator()
-    
-        nx, ny = intersection_in_geo_equator(eq, xx, yy)
-        xx += abs(nx - middle(xx))
-        yy += abs(ny - middle(yy))
-        
-        return xx, yy
-    
-    else:
-        return xx, yy
+            
+    return xx, yy
 
 
 def load_equator(infile = "database/GEO/dip_2013.txt"):
@@ -125,3 +184,59 @@ def load_equator(infile = "database/GEO/dip_2013.txt"):
    
     return df.values
 
+def test_on_map():    
+    year = 2013
+    
+    from GEO.src.mapping import quick_map
+    import cartopy.crs as ccrs
+    import matplotlib.pyplot as plt
+    import settings as s
+    from FluxTube.src.mag import Apex
+    
+    fig, ax = plt.subplots(
+        figsize = (8, 8), 
+        dpi = 300, 
+        subplot_kw = 
+        {'projection': ccrs.PlateCarree()}
+        )
+    
+    s.config_labels(fontsize = 15)
+    
+    lat_lims = dict(min = -30, max = 15, stp = 5)
+    lon_lims = dict(min = -100, max = -30, stp = 10) 
+    
+    quick_map(ax, lon_lims, lat_lims)
+    
+    site = "car"
+    
+    glat, glon = sites[site]["coords"]
+    name = sites[site]["name"]
+    
+    ax.scatter(glon, glat,
+        marker = "^", 
+        s = 100,
+        color = "r",
+        label = name)
+    
+    alt = 300        
+    max_mlat = Apex(alt).apex_lat_base(base = 150)
+    
+    d, i = run_igrf(site = site)
+        
+    x, y  = compute_meridian(
+        lon = glon, 
+        alt = alt, 
+        max_lat = np.degrees(max_mlat),
+        year = year
+            )
+    
+    x, y = align_to_site(x, y, glon)
+    
+    x, y = align_to_equator(x, y, glon, glat)
+    
+    ax.plot(x, y, lw = 2, label = f"{alt} km")
+
+    ax.legend(loc = "upper right")
+    
+    
+test_on_map()
