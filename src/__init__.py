945195cac825073a64be08941c95440783a0bef1
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-05-02 20:49:28 -0300

    update init

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,4 @@
-from .core import sites, year_fraction, load_equator
-from .mapping import quick_map, map_attrs
-from .meridians import limit_hemisphere, split_meridian
+from .core import sites, year_fraction, load_equator, run_igrf
+from .mapping import quick_map, map_attrs, mag_equator
+from .meridians import limit_hemisphere, split_meridian, interpolate
+from .map_attrs import get_receivers_in_range, circle_range
