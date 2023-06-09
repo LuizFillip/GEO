d064be6c7f1d19f4a1ed8545f9ffdfc71bc0195d
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-06-09 01:59:59 -0300

    plot square are

--- src/map_attrs.py
+++ src/map_attrs.py
@@ -89,3 +89,21 @@ def get_receivers_in_range(
     return out
 
 
+def plot_square_area(ax, 
+                   lat_min = -12, 
+                   lat_max = -2, 
+                   lon_max = -32, 
+                   lon_min = -42):
+    
+    
+    """Plotting square area by coords limits"""
+    
+    ax.plot([lon_min, lon_max, 
+             lon_max, lon_min, lon_min], 
+            [lat_min, lat_min, lat_max, lat_max, lat_min],
+             color='black', 
+             linewidth = 4, 
+             marker='.',
+             transform=ccrs.PlateCarree(), 
+             )
+    return
