86123b12baf4f4f9b9e3744d2986a9f0968ec95f
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-08-08 14:22:30 -0300

    find receivers near from equator

---
+++ src/nearby_equator.py
@@ -0,0 +1,44 @@
+import json
+import GEO as g
+
+
+
+def distance_from_equator(
+        lon, lat, year = 2013
+        ):
+    eq = g.load_equator(year)
+    x, y = eq[:, 0], eq[:, 1]
+    min_x, min_y, min_d = g.compute_distance(
+        x, y, lon, lat)
+    return min_d
+
+
+def stations_near_of_equator(
+        year = 2015,
+        distance = 6, 
+        extra_sts = ['ceeu', 'ceft', 'rnna', 'pbjp']
+        ):
+    
+    infile = f'database/GEO/coords/{year}.json'
+    sites = json.load(open(infile))
+    
+    out = []
+    
+    for name, key in sites.items():
+        lon, lat, alt = tuple(key)
+        
+        min_d = distance_from_equator(
+                lon, 
+                lat, 
+                year = year
+                )
+        
+        if min_d < distance:
+          
+            out.append(name)
+            
+    return out + extra_sts
+
+list_stations = stations_near_of_equator()
+
+print(list_stations)
