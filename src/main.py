1e863cb367bdb2e0c8dc1c187eb671217c97aa54
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-10-25 16:30:27 -0300

    updates

--- src/main.py
+++ src/main.py
@@ -6,13 +6,15 @@ import GEO as gg
 site = 'jic'
 glat, glon = gg.sites[site]['coords']
     
-for year in tqdm(range(2013, 2022)):
+# for year in tqdm(range(2013, 2022)):
     
-    date = dt.datetime(year, 1, 1)
+year = 2022
     
-    gg.save_meridian(
-            date, 
-            glon, 
-            glat, 
-            site
-            )
+date = dt.datetime(year, 1, 1)
+
+gg.save_meridian(
+        date, 
+        glon, 
+        glat, 
+        site
+        )
