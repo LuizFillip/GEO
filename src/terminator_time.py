a0d1478b91d170479849650f9bb0df40e0505b4b
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-12-01 14:57:53 -0300

    check if is night by longitude

--- src/terminator_time.py
+++ src/terminator_time.py
@@ -3,7 +3,10 @@ from astral.sun import sun
 import GEO as gg
 import base as b
 import datetime as dt
-
+from datetime import datetime
+import pytz
+from timezonefinder import TimezoneFinder
+from astral import LocationInfo
 
 def dusk_time(
         dn,  
@@ -48,5 +51,36 @@ def dusk_from_site(
     return dusk
 
 
+def is_night(longitude, latitude, date_time):
+    tf = TimezoneFinder()
+    timezone_str = tf.timezone_at(lng=longitude, lat=latitude)
+    city_tz = pytz.timezone(timezone_str)
 
+    city = LocationInfo("City", "Country", timezone_str, latitude, longitude)
+    s = sun(city.observer, date=date_time, tzinfo=city_tz)
 
+    sunrise = s["sunrise"].astimezone(city_tz)
+    sunset = s["sunset"].astimezone(city_tz)
+    
+    date_time = date_time.astimezone(city_tz)
+
+    if sunrise < date_time < sunset:
+        return False  # It's day
+    else:
+        return True  # It's night
+
+# Example usage
+
+# def main():
+    
+#     longitude = -46.6333  # Replace with the desired longitude
+#     latitude = -23.5505  # Replace with the desired latitude
+#     date_time_to_check = datetime(2023, 12, 1, 8, 0, 0)  # Replace with your desired date and time
+    
+#     night = is_night(longitude, latitude, date_time_to_check)
+#     if night:
+#         print("It's night.")
+#     else:
+#         print("It's day.")
+
+# main()
